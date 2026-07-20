7485f50bfebc0d0e70d9c43bbb6f0e608e89c259
Elizabeth <lizbeepmc@gmail.com>
2026-07-20 03:03:03 +0300

    made main where the app runs

---
+++ main.py
@@ -0,0 +1,8 @@
+from student import Student
+
+student1 = Student("Tayo", 12, "tayo@yahoo.com",74, "python", 2, 12)
+student2 = Student("Bolu", 12, "tayo@yahoo.com", 4,"biology", 9,10)
+
+student1.display_info()
+print("-" * 100)
+student2.display_info()
--- student.py
+++ student.py
@@ -11,9 +11,3 @@ class Student(Person):
     def display_info(self):
         print(f"My name is {self.name}.I am {self.age}.My email address is {self.email}.My student Id is {self.student_Id}.I am studying {self.course}.I am in level {self.level}.I am in grade {self.grade}")
 
-student1 = Student("Tayo", 12, "tayo@yahoo.com",74, "python", 2, 12)
-student2 = Student("Bolu", 12, "tayo@yahoo.com", 4,"biology", 9,10)
-
-student1.display_info()
-print("-" * 100)
-student2.display_info()
