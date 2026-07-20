62ef0a46b0f37d19bd1098c419562dcb89c1a6fd
Elizabeth <lizbeepmc@gmail.com>
2026-07-20 13:27:18 +0300

    added methods for grade and update grade . In student.py I also added the objects and attrıbute reuıred to display new grade + dısplay info etc. Now Lawrence wıll take over to solve step 5, 6 and 7 while faith solves step 8, 9 and 10

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ student2 = Student("Bolu", 12, "tayo@yahoo.com", 4,"biology", 9,10)
 student1.display_info()
 print("-" * 100)
 student2.display_info()
+
+student1.get_grade()
+print("-" * 100)
+student2.get_grade()
+
+student1.update_grade(75)
+print("-" * 100)
+student2.update_grade(80)
--- student.py
+++ student.py
@@ -6,8 +6,29 @@ class Student(Person):
         self.student_Id = student_Id
         self.course = course
         self.level = level
-        self.grade = grade
+        self.__grade = grade
     
     def display_info(self):
-        print(f"My name is {self.name}.I am {self.age}.My email address is {self.email}.My student Id is {self.student_Id}.I am studying {self.course}.I am in level {self.level}.I am in grade {self.grade}")
+        print(f"My name is {self.name}.I am {self.age}.My email address is {self.email}.My student Id is {self.student_Id}.I am studying {self.course}.I am in level {self.level}.I am in grade {self.__grade}")
+    
+    def study(self):
+        print(f"I am studying {self.course}")
+
+    def get_grade(self):
+       return self.__grade
+
+    
+    def update_grade(self, new_grade):
+        if 0 <= new_grade <= 100:
+            self.__grade = new_grade
+            print("Grade updated successfully."
+                  )
+        else:
+            print("Invalid grade. Enter a value from 0 to 100")
+
+    def check_result(self):
+        pass
+
+    def perform_role(self):
+        pass
 
